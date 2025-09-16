d08e3b13aa63770fe96a3396cb30c12db0c5fe0d
Kai Mast <kai@kaimast.com>
2025-09-16 15:42:16 -0700

    fix: export lock guard

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,9 @@ pub mod std;
 #[cfg(feature = "tokio")]
 pub mod tokio;
 
-pub use lock_info::{lock_snapshots, GuardInfo, GuardKind, Location, LockInfo, LockKind};
+pub use lock_info::{
+    lock_snapshots, GuardInfo, GuardKind, Location, LockGuard, LockInfo, LockKind,
+};
 
 #[cfg(feature = "test")]
 pub use lock_info::clear_lock_infos;
